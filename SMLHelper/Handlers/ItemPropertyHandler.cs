875ea14e7a1b601632f907bca01bffa0eb9f1a6b
AlexejheroYTB <alexejhero@gmail.com>
2019-04-08 21:23:15 +0300

    Add docs

--- SMLHelper/Handlers/ItemPropertyHandler.cs
+++ SMLHelper/Handlers/ItemPropertyHandler.cs
@@ -18,6 +18,11 @@
 
         internal static readonly IDictionary<InventoryItem, object[]> InventoryItemProperties = new SelfCheckingDictionary<InventoryItem, object[]>("InventoryItemProperties");
 
+        /// <summary>
+        /// Adds a property to a <see cref="TechType"/> value
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/> value</param>
+        /// <param name="property">The property</param>
         void IItemPropertyHandler.AddProperty(TechType techType, object property)
         {
             if (TechTypeProperties.TryGetValue(techType, out object[] existingProperties))
@@ -40,6 +45,26 @@
             }
         }
 
+        /// <summary>
+        /// Determines whether a <see cref="TechType"/> value has a property
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/> value</param>
+        /// <param name="property">The property</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="TechType"/> has the property; otherwise, <c>false</c>.
+        /// </returns>
+
+        bool IItemPropertyHandler.HasProperty(TechType techType, object property)
+        {
+            if (!Main.TryGetProperties(techType, out object[] properties)) return false;
+            return properties.Contains(property);
+        }
+
+        /// <summary>
+        /// Removes a property from a <see cref="TechType"/> value
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/> value</param>
+        /// <param name="property">The property</param>
         void IItemPropertyHandler.RemoveProperty(TechType techType, object property)
         {
             if (TechTypeProperties.TryGetValue(techType, out object[] existingProperties))
@@ -54,16 +79,23 @@
             }
         }
 
-        bool IItemPropertyHandler.HasProperty(TechType techType, object property)
-        {
-            if (!Main.TryGetProperties(techType, out object[] properties)) return false;
-            return properties.Contains(property);
-        }
-
+        /// <summary>
+        /// Gets all of the properties of a <see cref="TechType"/> value
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/></param>
+        /// <param name="properties">The properties of the <see cref="TechType"/></param>
+        /// <returns>
+        ///   Whether or not the object had any properties
+        /// </returns>
         bool IItemPropertyHandler.TryGetProperties(TechType techType, out object[] properties)
         {
             if (TechTypeProperties.TryGetValue(techType, out object[] existingProperties))
             {
+                if (existingProperties == null || existingProperties.Count() <= 0)
+                {
+                    properties = null;
+                    return false;
+                }
                 properties = existingProperties;
                 return true;
             }
@@ -71,6 +103,11 @@
             return false;
         }
 
+        /// <summary>
+        /// Adds a property to an <see cref="InventoryItem"/> object
+        /// </summary>
+        /// <param name="item">The <see cref="InventoryItem"/> object</param>
+        /// <param name="property">The property</param>
         void IItemPropertyHandler.AddProperty(InventoryItem item, object property)
         {
             if (InventoryItemProperties.TryGetValue(item, out object[] existingProperties))
@@ -93,12 +130,25 @@
             }
         }
 
+        /// <summary>
+        /// Determines whether an <see cref="InventoryItem"/> object has a property
+        /// </summary>
+        /// <param name="item">The <see cref="InventoryItem"/> object</param>
+        /// <param name="property">The property</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="InventoryItem"/> has the property; otherwise, <c>false</c>.
+        /// </returns>
         bool IItemPropertyHandler.HasProperty(InventoryItem item, object property)
         {
             if (!Main.TryGetProperties(item, out object[] properties)) return false;
             return properties.Contains(property);
         }
 
+        /// <summary>
+        /// Removes a property from an <see cref="InventoryItem"/> object
+        /// </summary>
+        /// <param name="item">The <see cref="InventoryItem"/></param>
+        /// <param name="property">The property</param>
         void IItemPropertyHandler.RemoveProperty(InventoryItem item, object property)
         {
             if (InventoryItemProperties.TryGetValue(item, out object[] existingProperties))
@@ -113,10 +163,23 @@
             }
         }
 
+        /// <summary>
+        /// Gets all of the properties of an <see cref="InventoryItem"/> object
+        /// </summary>
+        /// <param name="item">The <see cref="InventoryItem"/></param>
+        /// <param name="properties">The properties of the <see cref="InventoryItem"/></param>
+        /// <returns>
+        ///   Whether or not the object had any properties
+        /// </returns>
         bool IItemPropertyHandler.TryGetProperties(InventoryItem item, out object[] properties)
         {
             if (InventoryItemProperties.TryGetValue(item, out object[] existingProperties))
             {
+                if (existingProperties == null || existingProperties.Count() <= 0)
+                {
+                    properties = null;
+                    return false;
+                }
                 properties = existingProperties;
                 return true;
             }
@@ -126,44 +189,97 @@
 
         #region Static Methods
 
+        /// <summary>
+        /// Adds a property to a <see cref="TechType"/> value
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/> value</param>
+        /// <param name="property">The property</param>
         public static void AddProperty(TechType techType, object property)
         {
             Main.AddProperty(techType, property);
         }
 
-        public static void RemoveProperty(TechType techType, object property)
-        {
-            Main.RemoveProperty(techType, property);
-        }
+        /// <summary>
+        /// Determines whether a <see cref="TechType"/> value has a property
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/> value</param>
+        /// <param name="property">The property</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="TechType"/> has the property; otherwise, <c>false</c>.
+        /// </returns>
 
         public static bool HasProperty(TechType techType, object property)
         {
             return Main.HasProperty(techType, property);
         }
 
-        public static bool TryGetProperties(TechType techType, out object[] Properties)
+        /// <summary>
+        /// Removes a property from a <see cref="TechType"/> value
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/> value</param>
+        /// <param name="property">The property</param>
+        public static void RemoveProperty(TechType techType, object property)
         {
-            return Main.TryGetProperties(techType, out Properties);
+            Main.RemoveProperty(techType, property);
         }
 
+        /// <summary>
+        /// Gets all of the properties of a <see cref="TechType"/> value
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/></param>
+        /// <param name="properties">The properties of the <see cref="TechType"/></param>
+        /// <returns>
+        ///   Whether or not the object had any properties
+        /// </returns>
+        public static bool TryGetProperties(TechType techType, out object[] properties)
+        {
+            return Main.TryGetProperties(techType, out properties);
+        }
+
+        /// <summary>
+        /// Adds a property to an <see cref="InventoryItem"/> object
+        /// </summary>
+        /// <param name="item">The <see cref="InventoryItem"/> object</param>
+        /// <param name="property">The property</param>
         public static void AddProperty(InventoryItem item, object property)
         {
             Main.AddProperty(item, property);
         }
 
+        /// <summary>
+        /// Determines whether an <see cref="InventoryItem"/> object has a property
+        /// </summary>
+        /// <param name="item">The <see cref="InventoryItem"/> object</param>
+        /// <param name="property">The property</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="InventoryItem"/> has the property; otherwise, <c>false</c>.
+        /// </returns>
         public static bool HasProperty(InventoryItem item, object property)
         {
             return Main.HasProperty(item, property);
         }
 
+        /// <summary>
+        /// Removes a property from an <see cref="InventoryItem"/> object
+        /// </summary>
+        /// <param name="item">The <see cref="InventoryItem"/></param>
+        /// <param name="property">The property</param>
         public static void RemoveProperty(InventoryItem item, object property)
         {
             Main.RemoveProperty(item, property);
         }
 
-        public static bool TryGetProperties(InventoryItem item, out object[] Properties)
+        /// <summary>
+        /// Gets all of the properties of an <see cref="InventoryItem"/> object
+        /// </summary>
+        /// <param name="item">The <see cref="InventoryItem"/></param>
+        /// <param name="properties">The properties of the <see cref="InventoryItem"/></param>
+        /// <returns>
+        ///   Whether or not the object had any properties
+        /// </returns>
+        public static bool TryGetProperties(InventoryItem item, out object[] properties)
         {
-            return Main.TryGetProperties(item, out Properties);
+            return Main.TryGetProperties(item, out properties);
         }
 
         #endregion
--- SMLHelper/Interfaces/IItemPropertyHandler.cs
+++ SMLHelper/Interfaces/IItemPropertyHandler.cs
@@ -2,20 +2,72 @@
 {
     public interface IItemPropertyHandler
     {
+        /// <summary>
+        /// Adds a property to a <see cref="TechType"/> value
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/> value</param>
+        /// <param name="property">The property</param>
         void AddProperty(TechType techType, object property);
 
-        void RemoveProperty(TechType techType, object property);
-
+        /// <summary>
+        /// Determines whether a <see cref="TechType"/> value has a property
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/> value</param>
+        /// <param name="property">The property</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="TechType"/> has the property; otherwise, <c>false</c>.
+        /// </returns>
         bool HasProperty(TechType techType, object property);
 
+        /// <summary>
+        /// Removes a property from a <see cref="TechType"/> value
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/> value</param>
+        /// <param name="property">The property</param>
+        void RemoveProperty(TechType techType, object property);
+
+        /// <summary>
+        /// Gets all of the properties of a <see cref="TechType"/> value
+        /// </summary>
+        /// <param name="techType">The <see cref="TechType"/></param>
+        /// <param name="properties">The properties of the <see cref="TechType"/></param>
+        /// <returns>
+        ///   Whether or not the object had any properties
+        /// </returns>
         bool TryGetProperties(TechType techType, out object[] properties);
 
+        /// <summary>
+        /// Adds a property to an <see cref="InventoryItem"/> object
+        /// </summary>
+        /// <param name="item">The <see cref="InventoryItem"/> object</param>
+        /// <param name="property">The property</param>
         void AddProperty(InventoryItem item, object property);
 
+        /// <summary>
+        /// Determines whether an <see cref="InventoryItem"/> object has a property
+        /// </summary>
+        /// <param name="item">The <see cref="InventoryItem"/> object</param>
+        /// <param name="property">The property</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="InventoryItem"/> has the property; otherwise, <c>false</c>.
+        /// </returns>
         bool HasProperty(InventoryItem item, object property);
 
+        /// <summary>
+        /// Removes a property from an <see cref="InventoryItem"/> object
+        /// </summary>
+        /// <param name="item">The <see cref="InventoryItem"/></param>
+        /// <param name="property">The property</param>
         void RemoveProperty(InventoryItem item, object property);
 
+        /// <summary>
+        /// Gets all of the properties of an <see cref="InventoryItem"/> object
+        /// </summary>
+        /// <param name="item">The <see cref="InventoryItem"/></param>
+        /// <param name="properties">The properties of the <see cref="InventoryItem"/></param>
+        /// <returns>
+        ///   Whether or not the object had any properties
+        /// </returns>
         bool TryGetProperties(InventoryItem item, out object[] properties);
     }
 }
