19296b3983768a04033e2355cd49b8181428a6fb
PrimeSonic <prime.sonic@gmail.com>
2019-05-18 12:34:46 -0400

    LangOverrideBug - Reorganizing unit tests for more and clearer combinations

--- SMLHelper.Tests/LanguagePatcherTests.cs
+++ SMLHelper.Tests/LanguagePatcherTests.cs
@@ -8,31 +8,49 @@
     [TestFixture]
     public class LanguagePatcherTests
     {
-        private static readonly IEnumerable<string> CustomValues1 = new string[]
+        private static readonly IEnumerable<string> CustomValues = new string[]
         {
-            "CustomValue1", "CustomValue1{0}", "{0}CustomValue1", "{0}CustomValue1{1}",
-            "{0}Custom{1}Value1{2}", "Custom{0}Value1", "Custom\nValue1", "\nCustomValue1",
-            "CustomValue1\n", "\nCustom\nValue1\n", "Custom{0}\n{1}Value1",
-            "Custom-Value1\n", "\nCustom_Value1\n", "Custom{0}:{1}Value1",
+            // No special tokens
+            "CustomValue",
+            "CustomValue1",
+            "2Custom:Value1",
+            "CustomValue%",
+            // string.format tokens
+            "CustomValue{0}",
+            "{0}CustomValue",
+            "{0}CustomValue{1}",
+            "{0}Custom{1}Value{2}",
+            "Custom{0}Value",
+            // With Unity line breaks
+            "Custom\nValue",
+            "\nCustomValue",
+            "CustomValue\n",
+            "\nCustom\nValue\n",
+            // With mix
+            "Custom:{0}\n{1}:Value;",
+            "Custom-Value\n{0}",
+            "#1\nCustom_Value\n",
+            "Custom{0}:{1}%Value%",
         };
 
-        private static readonly IEnumerable<string> CustomValues2 = new string[]
+        private static readonly IEnumerable<string> LineEndings = new string[]
         {
-            "2CustomValue", "2CustomValue{0}", "{0}2CustomValue", "{0}2CustomValue{1}",
-            "{0}2Custom{1}Value{2}", "2Custom{0}Value", "2Custom\nValue", "\n2CustomValue",
-            "2CustomValue\n", "2\nCustom\nValue\n", "2Custom{0}\n{1}Value",
-            "2Custom-Value\n", "2\nCustom_Value\n", "2Custom{0}:{1}Value",
+            new string(new char[]{'\n' }),
+            new string(new char[]{'\r', '\n' }),
+            string.Empty,
         };
 
-        [TestCaseSource(nameof(CustomValues1))]
-        public void ExtractCustomLinesFromText_WhenTextIsValid_SingleEntry_KeyIsKnown_Overrides(string customValue)
+        [Test, Combinatorial]
+        public void ExtractCustomLinesFromText_WhenTextIsValid_SingleEntry_KeyIsKnown_Overrides(
+            [ValueSource(nameof(LineEndings))] string endOfLine,
+            [ValueSource(nameof(CustomValues))] string customValue)
         {
             var originalLines = new Dictionary<string, string>
             {
                 { "Key", "OriginalValue" }
             };
 
-            string text = "Key:{" + customValue + "}";
+            string text = "Key:{" + customValue + "}" + endOfLine;
 
             Console.WriteLine("TestText");
             Console.WriteLine(text);
@@ -45,9 +63,9 @@
 
         [Test, Combinatorial]
         public void ExtractCustomLinesFromText_WhenTextIsValid_MultipleEntries_KeyIsKnown_Overrides(
-            [Values("\n", "\r\n")] string endOfLine,
-            [ValueSource(nameof(CustomValues1))] string customValue1,
-            [ValueSource(nameof(CustomValues2))] string customValue2)
+            [ValueSource(nameof(LineEndings))] string endOfLine,
+            [ValueSource(nameof(CustomValues))] string customValue1,
+            [ValueSource(nameof(CustomValues))] string customValue2)
         {
             var originalLines = new Dictionary<string, string>
             {
