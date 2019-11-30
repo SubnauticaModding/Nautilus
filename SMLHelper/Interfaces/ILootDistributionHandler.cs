432ef5cab47647dd31f7cb5c09957ff271a79aad
ahk1221 <ahmed16.ahk@gmail.com>
2019-11-30 20:27:02 +0500

    FragmentHandling - Fixing some stuff

--- SMLHelper/Interfaces/ILootDistributionHandler.cs
+++ SMLHelper/Interfaces/ILootDistributionHandler.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SMLHelper.V2.Interfaces
+﻿namespace SMLHelper.V2.Interfaces
 {
-    class ILootDistributionHandler
+    /// <summary>
+    /// A handler that manages Loot Distribution (spawning of fragments, fish, etc).
+    /// </summary>
+    public interface ILootDistributionHandler
     {
+        /// <summary>
+        /// Adds in a custom entry into the Loot Distribution of the game.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="classId"></param>
+        void AddCustomLootDistData(LootDistributionData.SrcData data, string classId);
     }
 }
--- SMLHelper/Patchers/WorldEntityDatabasePatcher.cs
+++ SMLHelper/Patchers/WorldEntityDatabasePatcher.cs
@@ -1,6 +1,7 @@
 ﻿namespace SMLHelper.V2.Patchers
 {
     using Harmony;
+    using System;
     using UWE;
 
     internal class WorldEntityDatabasePatcher
@@ -9,15 +10,9 @@
 
         internal static void Patch(HarmonyInstance harmony)
         {
-            harmony.Patch(AccessTools.Constructor(typeof(WorldEntityDatabase)),
-                postfix: new HarmonyMethod(AccessTools.Method(typeof(WorldEntityDatabasePatcher), "CtorPostfix")));
-        }
-
-        private static void CtorPostfix()
-        {
-            foreach(var customInfo in CustomWorldEntityInfos)
+            foreach (var customInfo in CustomWorldEntityInfos)
             {
-                if(WorldEntityDatabase.main.infos.ContainsKey(customInfo.Key))
+                if (WorldEntityDatabase.main.infos.ContainsKey(customInfo.Key))
                 {
                     // TODO:    Allow some sort of functionality
                     //          that allows editing of existing entries
@@ -27,6 +22,14 @@
                     WorldEntityDatabase.main.infos.Add(customInfo);
                 }
             }
+
+            harmony.Patch(AccessTools.Constructor(typeof(WorldEntityDatabase)),
+                postfix: new HarmonyMethod(AccessTools.Method(typeof(WorldEntityDatabasePatcher), "CtorPostfix")));
+        }
+
+        private static void CtorPostfix()
+        {
+
         }
     }
 }
