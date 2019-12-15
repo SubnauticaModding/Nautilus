318d97af74b14a78199c0814d7bb15ce31361a36
PrimeSonic <prime.sonic@gmail.com>
2019-12-15 09:41:03 -0500

    BZ_Experimental - Added #if sections for the different builds

--- SMLHelper/Patchers/SpritePatcher.cs
+++ SMLHelper/Patchers/SpritePatcher.cs
@@ -9,21 +9,31 @@
         {
             // Direct access to private fields made possible by https://github.com/CabbageCrow/AssemblyPublicizer/
             // See README.md for details.
-
-            Dictionary<SpriteManager.Group, string> mapping = SpriteManager.mapping;
-            Dictionary<string, Dictionary<string, Atlas.Sprite>> atlases = SpriteManager.atlases;
+#if SUBNAUTICA
+            Dictionary<SpriteManager.Group, Dictionary<string, Atlas.Sprite>> groups = SpriteManager.groups;
 
             foreach (SpriteManager.Group moddedGroup in ModSprite.ModSprites.Keys)
             {
-                string groupName = mapping[moddedGroup];
-                Dictionary<string, Atlas.Sprite> spriteGroup = atlases[groupName];
-
+                Dictionary<string, Atlas.Sprite> spriteGroup = groups[moddedGroup];
                 foreach (string spriteKey in ModSprite.ModSprites[moddedGroup].Keys)
                 {
                     spriteGroup.Add(spriteKey, ModSprite.ModSprites[moddedGroup][spriteKey]);
                 }
             }
+#elif BELOWZERO
+            Dictionary<SpriteManager.Group, string> mapping = SpriteManager.mapping;
+            Dictionary<string, Dictionary<string, Atlas.Sprite>> atlases = SpriteManager.atlases;            
 
+            foreach (SpriteManager.Group moddedGroup in ModSprite.ModSprites.Keys)
+            {
+                string groupName = mapping[moddedGroup];
+                Dictionary<string, Atlas.Sprite> spriteGroup = atlases[groupName];
+                foreach (string spriteKey in ModSprite.ModSprites[moddedGroup].Keys)
+                {
+                    spriteGroup.Add(spriteKey, ModSprite.ModSprites[moddedGroup][spriteKey]);
+                }
+            }
+#endif
             Logger.Log("SpritePatcher is done.", LogLevel.Debug);
         }
     }
