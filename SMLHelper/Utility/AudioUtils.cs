937d5ac8abe3b0ec67c0601812f0e775eefc426d
AlexejheroYTB <alexejhero@gmail.com>
2020-01-04 21:31:07 +0200

    audio - Split method into 3

--- SMLHelper/Utility/AudioUtils.cs
+++ SMLHelper/Utility/AudioUtils.cs
@@ -1,6 +1,7 @@
 ﻿namespace SMLHelper.V2.Utility
 {
     using FMOD;
+    using FMODUnity;
 
     /// <summary>
     /// Utilities for audio and sound
@@ -8,19 +9,36 @@
     public static class AudioUtils
     {
         /// <summary>
-        /// Plays a sound globally. Must be a .wav file
+        /// Creates a <see cref="Sound"/> instance from a path. Can be stored and later used with <see cref="PlaySound(Sound)"/>
         /// </summary>
         /// <param name="path">The path of the sound. Relative to the base game folder.</param>
-        /// <returns></returns>
-        public static RESULT PlaySound(string path)
+        /// <returns>The <see cref="Sound"/> instance</returns>
+        public static Sound CreateSound(string path)
         {
-            Sound newSound = new Sound();
-            ChannelGroup channels = new ChannelGroup();
-            Channel newChannel = new Channel();
-            var system = FMODUnity.RuntimeManager.LowlevelSystem;
-            system.getMasterChannelGroup(out channels);
-            system.createSound(path, MODE.DEFAULT, out newSound);
-            return system.playSound(newSound, channels, false, out newChannel);
+            RuntimeManager.LowlevelSystem.createSound(path, MODE.DEFAULT, out Sound sound);
+            return sound;
+        }
+
+        /// <summary>
+        /// Plays a <see cref="Sound"/> globally from a path. Must be a .wav file
+        /// </summary>
+        /// <param name="path">The path of the sound. Relative to the base game folder.</param>
+        /// <returns>The channel on which the sound was created</returns>
+        public static Channel PlaySound(string path)
+        {
+            return PlaySound(CreateSound(path));
+        }
+
+        /// <summary>
+        /// Plays a <see cref="Sound"/> globally
+        /// </summary>
+        /// <param name="sound">The sound which should be played</param>
+        /// <returns>The channel on which the sound was created</returns>
+        public static Channel PlaySound(Sound sound)
+        {
+            RuntimeManager.LowlevelSystem.getMasterChannelGroup(out ChannelGroup channels);
+            RuntimeManager.LowlevelSystem.playSound(sound, channels, false, out Channel newChannel);
+            return newChannel;
         }
     }
 }
