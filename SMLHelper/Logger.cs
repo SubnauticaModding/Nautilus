696a02b6fe152f835aa4e3e882a024c1a46a13d3
AlexejheroYTB <alexejhero@gmail.com>
2020-01-06 13:48:57 +0200

    Revert Logger.cs

--- SMLHelper/Logger.cs
+++ SMLHelper/Logger.cs
@@ -14,6 +14,49 @@
 
     internal static class Logger
     {
+        internal static bool Initialized = false;
+
+        internal static bool EnableDebugging { get; private set; }
+        internal static void SetDebugging(bool value)
+        {
+            string configPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "EnableDebugLogs.txt");
+
+            File.WriteAllText(configPath, value.ToString());
+            EnableDebugging = value;
+        }
+
+        internal static void Initialize()
+        {
+            if (Initialized) return;
+            Initialized = true;
+
+            string configPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "EnableDebugLogs.txt");
+
+            if (!File.Exists(configPath))
+            {
+                File.WriteAllText(configPath, "False");
+                EnableDebugging = false;
+
+                return;
+            }
+
+            string fileContents = File.ReadAllText(configPath);
+
+            try
+            {
+                EnableDebugging = bool.Parse(fileContents);
+
+                Log($"Enable debug logs set to: {EnableDebugging.ToString()}", LogLevel.Info);
+            }
+            catch (Exception)
+            {
+                File.WriteAllText(configPath, "False");
+                EnableDebugging = false;
+
+                Log("Error reading EnableDebugLogs.txt configuration file. Defaulted to false", LogLevel.Warn);
+            }
+        }
+                
         internal static void Debug(string text) => Log(text, LogLevel.Debug);
         internal static void Info(string text) => Log(text, LogLevel.Info);
         internal static void Warn(string text) => Log(text, LogLevel.Warn);
@@ -26,19 +69,27 @@
 
         internal static void Log(string text, LogLevel level = LogLevel.Info)
         {
-            Console.WriteLine($"[SMLHelper/{level.ToString()}] {text}");
+            Initialize();
+
+            if (level >= LogLevel.Info || EnableDebugging)
+                Console.WriteLine($"[SMLHelper/{level.ToString()}] {text}");
         }
 
         internal static void Log(string text, LogLevel level = LogLevel.Info, params object[] args)
         {
+            Initialize();
+
             if (args != null && args.Length > 0)
                 text = string.Format(text, args);
 
-            Console.WriteLine($"[SMLHelper/{level.ToString()}] {text}");
+            if (level >= LogLevel.Info || EnableDebugging)
+                Console.WriteLine($"[SMLHelper/{level.ToString()}] {text}");
         }
 
         internal static void Announce(string text, LogLevel level = LogLevel.Info, bool logToFile = false)
         {
+            Initialize();
+
             ErrorMessage.AddMessage(text);
 
             if (logToFile)
@@ -47,6 +98,8 @@
 
         internal static void Announce(string text, LogLevel level = LogLevel.Info, bool logToFile = false, params object[] args)
         {
+            Initialize();
+
             ErrorMessage.AddMessage(string.Format(text, args));
 
             if (logToFile)
