a7383ba2c10dec53b6060936ac3ac1331b8c7a66
zorgesho <53096970+zorgesho@users.noreply.github.com>
2020-02-06 15:27:22 +0300

    patch for collapsing/expanding mod options

--- SMLHelper/Patchers/OptionsPanelPatcher.cs
+++ SMLHelper/Patchers/OptionsPanelPatcher.cs
@@ -7,14 +7,17 @@
     using System.Collections.Generic;
     using UnityEngine;
     using UnityEngine.Events;
+    using UnityEngine.EventSystems;
     using UnityEngine.UI;
     using QModManager.API;
+
+    using Object = UnityEngine.Object;
 
     internal class OptionsPanelPatcher
     {
         internal static SortedList<string, ModOptions> modOptions = new SortedList<string, ModOptions>();
 
-        private static int  modsTab = -1; // index of 'Mods' tab
+        private static int  modsTabIndex = -1;
         private static bool isMainMenu = true; // is options was opened in main menu or in game
 
         internal static void Patch(HarmonyInstance harmony)
@@ -24,7 +27,20 @@
             if (QModServices.Main.FindModById("ModsOptionsAdjusted")?.Enable ?? false)
                 V2.Logger.Log("ModOptionsAdjuster is not inited (ModsOptionsAdjusted mod is active)", LogLevel.Warn);
             else
+            {
                 PatchUtils.PatchClass(harmony, typeof(ModOptionsAdjuster));
+                PatchUtils.PatchClass(harmony, typeof(ModOptionsHeadingsToggle));
+            }
+        }
+
+
+        // 'Mods' tab also added in QModManager, so we can't rely on 'modsTab' in AddTabs_Postfix
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(uGUI_OptionsPanel), nameof(uGUI_OptionsPanel.AddTab))]
+        internal static void AddTab_Postfix(string label, int __result)
+        {
+            if (label == "Mods")
+                modsTabIndex = __result;
         }
 
         [HarmonyPostfix]
@@ -35,7 +51,7 @@
             isMainMenu = (optionsPanel.GetComponent<MainMenuOptions>() != null);
 
             // Start the modsTab index at a value of -1
-            modsTab = -1;
+            int modsTab = -1;
             // Loop through all of the tabs
             for (int i = 0; i < optionsPanel.tabsContainer.childCount; i++)
             {
@@ -125,7 +141,7 @@
             [HarmonyPatch(typeof(uGUI_TabbedControlsPanel), nameof(uGUI_TabbedControlsPanel.AddItem), new Type[] { typeof(int), typeof(GameObject) })]
             private static void AddItem_Postfix(int tabIndex, GameObject __result)
             {
-                if (__result == null || tabIndex != modsTab)
+                if (__result == null || tabIndex != modsTabIndex)
                     return;
 
                 foreach (var type in optionTypes)
@@ -265,6 +281,207 @@
                     Destroy(this);
                 }
             }
+        }
+
+
+        // Class for collapsing/expanding options in 'Mods' tab
+        // Options can be collapsed/expanded by clicking on mod's title or arrow button
+        private static class ModOptionsHeadingsToggle
+        {
+            private enum HeadingState { Collapsed, Expanded };
+
+            private static GameObject headingPrefab = null;
+
+            private static class StoredHeadingStates
+            {
+                private class StatesConfig
+                {
+                    private readonly Dictionary<string, HeadingState> states = new Dictionary<string, HeadingState>();
+
+                    public HeadingState this[string name]
+                    {
+                        get => states.TryGetValue(name, out HeadingState state)? state: HeadingState.Expanded;
+
+                        set
+                        {
+                            states[name] = value;
+                            // TODO: save
+                        }
+                    }
+                }
+                private static readonly StatesConfig statesConfig = new StatesConfig(); // TODO: load
+
+                public static HeadingState get(string name) => statesConfig[name];
+                public static void store(string name, HeadingState state) => statesConfig[name] = state;
+            }
+
+            // we add arrow button from Choice ui element to the options headings for collapsing/expanding 
+            private static void InitHeadingPrefab(uGUI_TabbedControlsPanel panel)
+            {
+                if (headingPrefab)
+                    return;
+
+                headingPrefab = Object.Instantiate(panel.headingPrefab);
+                headingPrefab.name = "OptionHeadingToggleable";
+                headingPrefab.AddComponent<HeadingToggle>();
+
+                Transform captionTransform = headingPrefab.transform.Find("Caption");
+                captionTransform.localPosition = new Vector3(45f, 0f, 0f);
+                captionTransform.gameObject.AddComponent<HeadingClickHandler>();
+                captionTransform.gameObject.AddComponent<ContentSizeFitter>().horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+
+                GameObject button = Object.Instantiate(panel.choiceOptionPrefab.transform.Find("Choice/Background/NextButton").gameObject);
+                button.name = "HeadingToggleButton";
+                button.AddComponent<ToggleButtonClickHandler>();
+
+                RectTransform buttonTransform = button.transform as RectTransform;
+                buttonTransform.SetParent(headingPrefab.transform);
+                buttonTransform.SetAsFirstSibling();
+                buttonTransform.localEulerAngles = new Vector3(0f, 0f, -90f);
+                buttonTransform.localPosition = new Vector3(15f, -13f, 0f);
+                buttonTransform.pivot = new Vector2(0.25f, 0.5f);
+                buttonTransform.anchorMin = buttonTransform.anchorMax = new Vector2(0f, 0.5f);
+            }
+
+            #region components
+            // main component for headings toggling
+            private class HeadingToggle: MonoBehaviour
+            {
+                private HeadingState headingState = HeadingState.Expanded;
+                private string headingName = null;
+                private List<GameObject> childOptions = null;
+
+                private void Init()
+                {
+                    if (childOptions != null)
+                        return;
+
+                    headingName = transform.Find("Caption")?.GetComponent<Text>()?.text;
+
+                    childOptions = new List<GameObject>();
+
+                    for (int i = transform.GetSiblingIndex() + 1; i < transform.parent.childCount; i++)
+                    {
+                        GameObject option = transform.parent.GetChild(i).gameObject;
+
+                        if (option.GetComponent<HeadingToggle>())
+                            break;
+
+                        childOptions.Add(option);
+                    }
+                }
+
+                public void EnsureState() // for setting previously saved state
+                {
+                    Init();
+
+                    HeadingState storedState = StoredHeadingStates.get(headingName);
+
+                    if (headingState != storedState)
+                    {
+                        SetState(storedState);
+                        GetComponentInChildren<ToggleButtonClickHandler>()?.SetStateInstant(storedState);
+                    }
+                }
+
+                public void SetState(HeadingState state)
+                {
+                    Init();
+
+                    childOptions.ForEach(option => option.SetActive(state == HeadingState.Expanded));
+                    headingState = state;
+
+                    StoredHeadingStates.store(headingName, state);
+                }
+            }
+
+            // click handler for arrow button
+            private class ToggleButtonClickHandler: MonoBehaviour, IPointerClickHandler
+            {
+                private const float timeRotate = 0.1f;
+                private HeadingState headingState = HeadingState.Expanded;
+                private bool isRotating = false;
+
+                public void SetStateInstant(HeadingState state)
+                {
+                    headingState = state;
+                    transform.localEulerAngles = new Vector3(0, 0, headingState == HeadingState.Expanded? -90: 0);
+                }
+
+                public void OnPointerClick(PointerEventData _)
+                {
+                    if (isRotating)
+                        return;
+
+                    headingState = headingState == HeadingState.Expanded? HeadingState.Collapsed: HeadingState.Expanded;
+                    StartCoroutine(SmoothRotate(headingState == HeadingState.Expanded? -90: 90));
+
+                    GetComponentInParent<HeadingToggle>()?.SetState(headingState);
+                }
+
+                private IEnumerator SmoothRotate(float angles)
+                {
+                    isRotating = true;
+
+                    Quaternion startRotation = transform.localRotation;
+                    Quaternion endRotation = Quaternion.Euler(new Vector3(0f, 0f, angles)) * startRotation;
+
+                    float timeStart = Time.realtimeSinceStartup; // Time.deltaTime works only in main menu
+
+                    while (timeStart + timeRotate > Time.realtimeSinceStartup)
+                    {
+                        transform.localRotation = Quaternion.Lerp(startRotation, endRotation, (Time.realtimeSinceStartup - timeStart) / timeRotate);
+                        yield return null;
+                    }
+
+                    transform.localRotation = endRotation;
+                    isRotating = false;
+                }
+            }
+
+            // click handler for title, just redirects clicks to button click handler
+            private class HeadingClickHandler: MonoBehaviour, IPointerClickHandler
+            {
+                public void OnPointerClick(PointerEventData eventData) =>
+                    transform.parent.GetComponentInChildren<ToggleButtonClickHandler>()?.OnPointerClick(eventData);
+            }
+            #endregion
+
+            #region patches for uGUI_TabbedControlsPanel
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(uGUI_TabbedControlsPanel), nameof(uGUI_TabbedControlsPanel.AddHeading))]
+            private static bool AddHeading_Prefix(uGUI_TabbedControlsPanel __instance, int tabIndex, string label)
+            {
+                if (tabIndex != modsTabIndex)
+                    return true;
+
+                __instance.AddItem(tabIndex, headingPrefab, label);
+                return false;
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(uGUI_TabbedControlsPanel), nameof(uGUI_TabbedControlsPanel.Awake))]
+            private static void Awake_Postfix(uGUI_TabbedControlsPanel __instance)
+            {
+                InitHeadingPrefab(__instance);
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(uGUI_TabbedControlsPanel), nameof(uGUI_TabbedControlsPanel.SetVisibleTab))]
+            private static void SetVisibleTab_Prefix(uGUI_TabbedControlsPanel __instance, int tabIndex)
+            {
+                if (tabIndex != modsTabIndex)
+                    return;
+
+                // just in case, for changing vertical spacing between ui elements
+                //__instance.tabs[tabIndex].container.GetComponent<VerticalLayoutGroup>().spacing = 15f; // default is 15f
+
+                Transform options = __instance.tabs[tabIndex].container.transform;
+
+                for (int i = 0; i < options.childCount; i++)
+                    options.GetChild(i).GetComponent<HeadingToggle>()?.EnsureState();
+            }
+            #endregion
         }
     }
 }
