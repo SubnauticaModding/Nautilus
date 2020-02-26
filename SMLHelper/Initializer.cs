47e05a546da3c32b73b87e4ad7ee92fac303c9df
MrPurple6411 <mrpurple6411@gmail.com>
2020-02-26 15:49:44 -0700

    Preload Cache And AutoSave Cache.
    Also Included Cache Sorting by techtype number to make it easier to read when things are removed.

--- SMLHelper/Initializer.cs
+++ SMLHelper/Initializer.cs
@@ -20,12 +20,14 @@
         [Obsolete("This method is for use only by QModManager.", true)]
         public static void PrePatch()
         {
-            Logger.Initialize();
+            Logger.Initialize(); 
 #if SUBNAUTICA
             Logger.Log($"Loading v{Assembly.GetExecutingAssembly().GetName().Version} for Subnautica", LogLevel.Info);
 #elif BELOWZERO
             Logger.Log($"Loading v{Assembly.GetExecutingAssembly().GetName().Version} for BelowZero", LogLevel.Info);
 #endif
+            TechTypePatcher.cacheManager.LoadCache();
+            CraftTreeTypePatcher.cacheManager.LoadCache();
         }
 
         /// <summary>
@@ -69,6 +71,8 @@
             WorldEntityDatabasePatcher.Patch(harmony);
             IngameMenuPatcher.Patch(harmony);
             TooltipPatcher.Patch(harmony);
+            TechTypePatcher.cacheManager.SaveCache();
+            CraftTreeTypePatcher.cacheManager.SaveCache();
         }
     }
 }
