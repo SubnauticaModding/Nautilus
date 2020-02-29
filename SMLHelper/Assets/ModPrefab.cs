0d96bb071cdc75cd1ce24fd881590b4e75030ad8
MrPurple6411 <mrpurple6411@gmail.com>
2020-02-29 14:20:41 -0700

    This Was not actually required

--- SMLHelper/Assets/ModPrefab.cs
+++ SMLHelper/Assets/ModPrefab.cs
@@ -22,7 +22,6 @@
         }
 
         internal static IEnumerable<ModPrefab> Prefabs => PreFabsList;
-        internal static int PrefabCount = PreFabsList.Count;
         internal static bool TryGetFromFileName(string classId, out ModPrefab prefab) => FileNameDictionary.TryGetValue(classId, out prefab);
         internal static bool TryGetFromClassId(string classId, out ModPrefab prefab) => ClassIdDictionary.TryGetValue(classId, out prefab);
 
