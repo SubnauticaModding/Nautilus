8f0f487553933f445a35e7f70a0e913d53c2c28b
Tobey Blaber <45315526+toberandober@users.noreply.github.com>
2020-03-27 15:19:04 +0000

    Simple KeyCode converter for Newtonsoft.Json which reads and writes using KeyCodeUtils.KeyCodeToString/StringToKeyCode methods for readability.

---
+++ SMLHelper/Options/JsonConverters/KeyCodeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SMLHelper.V2.Utility;
+using Oculus.Newtonsoft.Json;
+using UnityEngine;
+
+namespace SMLHelper.V2.Options.JsonConverters
+{
+    internal class KeyCodeConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var keyCode = (KeyCode)value;
+            writer.WriteValue(KeyCodeUtils.KeyCodeToString(keyCode));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var s = (string)reader.Value;
+            return KeyCodeUtils.StringToKeyCode(s);
+        }
+
+        public override bool CanConvert(Type objectType) => objectType == typeof(KeyCode);
+    }
+}
