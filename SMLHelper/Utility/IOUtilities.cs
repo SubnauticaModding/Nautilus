4a8e1c81ef01452edd1bcafcf959e29bd94c78e2
O-S Marin <K07H@users.noreply.github.com>
2020-06-14 20:17:59 +0200

    Added a public getter to know if Debug logs are enabled.
    => Added a public getter to know if Debug logs are enabled.
    => Completed comments of existing method "Combine()".

--- SMLHelper/Utility/IOUtilities.cs
+++ SMLHelper/Utility/IOUtilities.cs
@@ -7,21 +7,26 @@
     /// </summary>
     public static class IOUtilities
     {
+        /// <summary>Used to know if Debug logs are enabled.</summary>
+        public static bool IsDebugLogsEnabled
+        {
+            get => Logger.EnableDebugging;
+            private set { }
+        }
+
         /// <summary>
         /// Works like <see cref="Path.Combine(string, string)"/>, but can have more than 2 paths
         /// </summary>
-        /// <param name="one"></param>
-        /// <param name="two"></param>
-        /// <param name="rest"></param>
-        /// <returns></returns>
+        /// <param name="one">The first part of the path.</param>
+        /// <param name="two">The second part of the path.</param>
+        /// <param name="rest">Optional: Additional path parts to concatenate at the end.</param>
+        /// <returns>Returns combined path parts as a single path.</returns>
         public static string Combine(string one, string two, params string[] rest)
         {
             string path = Path.Combine(one, two);
 
             foreach (string str in rest)
-            {
                 path = Path.Combine(path, str);
-            }
 
             return path;
         }
