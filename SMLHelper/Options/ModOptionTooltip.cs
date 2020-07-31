81de73d90e8da93eaf8c1e9be6377e3ab840d60f
Tobey Blaber <tobey@straitjacket.software>
2020-07-31 04:38:11 +0100

    Added a `ModOptionTooltip` class based on the `Tooltip` class from the SMLHelper example mod as a base for creating Tooltips internally.

---
+++ SMLHelper/Options/ModOptionTooltip.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SMLHelper.V2.Options
+{
+    internal class ModOptionTooltip : MonoBehaviour, ITooltip
+    {
+        public string Tooltip;
+
+        void Awake() => Destroy(GetComponent<LayoutElement>());
+
+        public void GetTooltip(out string tooltipText, List<TooltipIcon> _) => tooltipText = Tooltip;
+    }
+}
