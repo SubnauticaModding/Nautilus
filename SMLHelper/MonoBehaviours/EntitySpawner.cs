ece824d8a3a91f9eafb66e3f72d0a02e04aa3f69
Metious <71298690+Metious@users.noreply.github.com>
2021-04-28 18:18:33 +0430

    added a new MonoBehaviour for spawning

---
+++ SMLHelper/MonoBehaviours/EntitySpawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using SMLHelper.V2.Handlers;
+using UnityEngine;
+using UWE;
+
+namespace SMLHelper.V2.MonoBehaviours
+{
+    public class EntitySpawner : MonoBehaviour
+    {
+        public SpawnInfo spawnInfo;
+
+        void Start() => StartCoroutine(SpawnAsync());
+
+        IEnumerator SpawnAsync()
+        {
+            var task = new TaskResult<GameObject>();
+            yield return GetPrefabAsync(task);
+
+            var obj = UWE.Utils.InstantiateDeactivated(task.Get(), spawnInfo.spawnPosition, Quaternion.identity);
+            
+            LargeWorld.main.streamer.cellManager.RegisterEntity(obj);
+            
+            obj.SetActive(true);
+            
+            Destroy(gameObject);
+        }
+
+        IEnumerator GetPrefabAsync(IOut<GameObject> gameObject)
+        {
+            GameObject obj = null;
+            
+            if (!string.IsNullOrEmpty(spawnInfo.classId)) // if classid isn't null, then we get the prefab using it
+            {
+                var request = PrefabDatabase.GetPrefabAsync(spawnInfo.classId);
+                yield return request;
+
+                if (!request.TryGetPrefab(out obj))
+                    Logger.Error($"no prefab found for ClassID: {spawnInfo.classId}");
+            }
+            else if (spawnInfo.techType != TechType.None) // if techtype isn't null, then we get the prefab using it
+            {
+                var task = CraftData.GetPrefabForTechTypeAsync(spawnInfo.techType);
+                yield return task;
+
+                obj = task.GetResult();
+            }
+            
+            gameObject.Set(obj);
+        }
+    }
+}
