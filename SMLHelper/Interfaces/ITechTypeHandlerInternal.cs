a64853de0b83d443883df7485f28db837cc19e86
Metious <71298690+Metious@users.noreply.github.com>
2021-04-28 09:37:57 +0430

    Modified ITechTypeHandlerInternal methods arguments

--- SMLHelper/Interfaces/ITechTypeHandlerInternal.cs
+++ SMLHelper/Interfaces/ITechTypeHandlerInternal.cs
@@ -1,8 +1,10 @@
-﻿namespace SMLHelper.V2.Interfaces
+﻿using System.Reflection;
+
+namespace SMLHelper.V2.Interfaces
 {
     internal interface ITechTypeHandlerInternal : ITechTypeHandler
     {
-        TechType AddTechType(string modName, string internalName, string displayName, string tooltip);
-        TechType AddTechType(string modName, string internalName, string displayName, string tooltip, bool unlockAtStart);
+        TechType AddTechType(Assembly assembly, string internalName, string displayName, string tooltip);
+        TechType AddTechType(Assembly assembly, string internalName, string displayName, string tooltip, bool unlockAtStart);
     }
 }
