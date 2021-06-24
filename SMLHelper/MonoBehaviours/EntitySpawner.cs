8fb98285399973cb37a10f54b41e9844d1786b4b
Metious <71298690+Metious@users.noreply.github.com>
2021-06-24 11:19:31 +0430

    fixed non-global spawn bug

--- SMLHelper/MonoBehaviours/EntitySpawner.cs
+++ SMLHelper/MonoBehaviours/EntitySpawner.cs
@@ -26,13 +26,29 @@ namespace SMLHelper.V2.MonoBehaviours
             var task = new TaskResult<GameObject>();
             yield return GetPrefabAsync(task);
 
-            if (task.Get() is null)
+            var prefab = task.Get();
+
+            if (prefab == null)
             {
                 Logger.Error($"no prefab found for {stringToLog}; process for Coordinated Spawn canceled.");
                 Destroy(gameObject);
             }
 
-            var obj = Utils.InstantiateDeactivated(task.Get(), spawnInfo.spawnPosition, spawnInfo.rotation);
+            var lwe = prefab.GetComponent<LargeWorldEntity>();
+
+            // non-global objects cannot be spawned in unloaded terrain so we need to wait
+            if (lwe.cellLevel is not (LargeWorldEntity.CellLevel.Batch or LargeWorldEntity.CellLevel.Global))
+            {
+                var lws = LargeWorldStreamer.main;
+                while (!lws.IsReady()) // first we wait for the world streamer to be initialized
+                {
+                    yield return new WaitForSecondsRealtime(2f);
+                }
+                var batch = lws.GetContainingBatch(spawnInfo.spawnPosition);
+                yield return new WaitUntil(() => lws.IsBatchReadyToCompile(batch)); // then we wait until the terrain is fully loaded (must be checked on each frame for faster spawns)
+            }
+
+            var obj = Utils.InstantiateDeactivated(prefab, spawnInfo.spawnPosition, spawnInfo.rotation);
 
             LargeWorld.main.streamer.cellManager.RegisterEntity(obj);
             
