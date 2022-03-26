117fc0183ef914668578037a5f3f1049c6831b87
MrPurple6411 <mrpurple6411@gmail.com>
2022-03-26 15:01:13 -0600

    Add Compound Unlock capabilities to KnownTechHandler
    Also add ability to Remove Unlocks for techtypes both globally and from specific targeted types.

--- SMLHelper/Handlers/KnownTechHandler.cs
+++ SMLHelper/Handlers/KnownTechHandler.cs
@@ -4,6 +4,7 @@
     using Patchers;
     using System.Collections.Generic;
     using UnityEngine;
+    using Logger = V2.Logger;
 
     /// <summary>
     /// A handler class for configuring custom unlocking conditions for item blueprints.
@@ -42,25 +43,104 @@
 
         internal void AddAnalysisTech(TechType techTypeToBeAnalysed, IEnumerable<TechType> techTypesToUnlock, string UnlockMessage = "NotificationBlueprintUnlocked", FMODAsset UnlockSound = null, UnityEngine.Sprite UnlockSprite = null)
         {
-            if (KnownTechPatcher.AnalysisTech.TryGetValue(techTypeToBeAnalysed, out KnownTech.AnalysisTech existingEntry))
+            if (techTypeToBeAnalysed != TechType.None)
             {
-                existingEntry.unlockMessage = existingEntry.unlockMessage ?? UnlockMessage;
-                existingEntry.unlockSound = existingEntry.unlockSound ?? UnlockSound;
-                existingEntry.unlockPopup = existingEntry.unlockPopup ?? UnlockSprite;
-                existingEntry.unlockTechTypes.AddRange(techTypesToUnlock);
+                if (KnownTechPatcher.AnalysisTech.TryGetValue(techTypeToBeAnalysed, out KnownTech.AnalysisTech existingEntry))
+                {
+                    existingEntry.unlockMessage = existingEntry.unlockMessage ?? UnlockMessage;
+                    existingEntry.unlockSound = existingEntry.unlockSound ?? UnlockSound;
+                    existingEntry.unlockPopup = existingEntry.unlockPopup ?? UnlockSprite;
+                    existingEntry.unlockTechTypes.AddRange(techTypesToUnlock);
+                }
+                else
+                {
+                    KnownTechPatcher.AnalysisTech.Add(techTypeToBeAnalysed, new KnownTech.AnalysisTech()
+                    {
+                        techType = techTypeToBeAnalysed,
+                        unlockMessage = UnlockMessage,
+                        unlockSound = UnlockSound,
+                        unlockPopup = UnlockSprite,
+                        unlockTechTypes = new List<TechType>(techTypesToUnlock)
+                    });
+                }
             }
             else
             {
-                KnownTechPatcher.AnalysisTech.Add(techTypeToBeAnalysed, new KnownTech.AnalysisTech()
-                {
-                    techType = techTypeToBeAnalysed,
-                    unlockMessage = UnlockMessage,
-                    unlockSound = UnlockSound,
-                    unlockPopup = UnlockSprite,
-                    unlockTechTypes = new List<TechType>(techTypesToUnlock)
-                });
+                Logger.Error("Cannot Add Unlock to TechType.None!");
             }
         }
+
+        internal void AddCompoundUnlock(TechType techType, List<TechType> compoundTechsForUnlock)
+        {
+            if (techType == TechType.None)
+            {
+                Logger.Error("Cannot Add Unlock to TechType.None!");
+                return;
+            }
+
+            if (compoundTechsForUnlock.Contains(techType))
+            {
+                Logger.Error("Cannot Add Compound Unlock that contains itself!");
+                return;
+            }
+
+
+            if (KnownTechPatcher.CompoundTech.TryGetValue(techType, out KnownTech.CompoundTech compoundTech))
+            {
+                Logger.Debug($"Compound Unlock already found for {techType.AsString()}, Overwriting.");
+                compoundTech.dependencies = compoundTechsForUnlock;
+            }
+            else
+            {
+                Logger.Debug($"Adding Compound Unlock for {techType.AsString()}");
+                KnownTechPatcher.CompoundTech.Add(techType, new KnownTech.CompoundTech() { techType = techType, dependencies = compoundTechsForUnlock });
+            }
+        }
+
+        internal void RemoveAnalysisSpecific(TechType targetTechType, List<TechType> techTypes)
+        {
+            foreach (TechType techType in techTypes)
+            {
+                if (KnownTechPatcher.RemoveFromSpecificTechs.TryGetValue(techType, out var types))
+                {
+                    if (!types.Contains(targetTechType))
+                        types.Add(targetTechType);
+                }
+                else
+                {
+                    KnownTechPatcher.RemoveFromSpecificTechs[techType] = new List<TechType>() { targetTechType };
+                }
+            }
+        }
+
+        internal void RemoveAnalysisTechEntry(TechType targetTechType)
+        {
+            foreach (KnownTech.AnalysisTech tech in KnownTechPatcher.AnalysisTech.Values)
+            {
+                if (tech.unlockTechTypes.Contains(targetTechType))
+                {
+                    Logger.Debug($"Removed {targetTechType.AsString()} from {tech.techType.AsString()} unlocks that was added by another mod!");
+                    tech.unlockTechTypes.Remove(targetTechType);
+                }
+            }
+
+            if (KnownTechPatcher.CompoundTech.TryGetValue(targetTechType, out var types))
+            {
+                Logger.Debug($"Removed Compound Unlock for {targetTechType.AsString()} that was added by another mod!");
+                KnownTechPatcher.CompoundTech.Remove(targetTechType);
+            }
+
+            if (KnownTechPatcher.UnlockedAtStart.Contains(targetTechType))
+            {
+                Logger.Debug($"Removed UnlockedAtStart for {targetTechType.AsString()} that was added by another mod!");
+                KnownTechPatcher.UnlockedAtStart.Remove(targetTechType);
+            }
+
+            if (!KnownTechPatcher.RemovalTechs.Contains(targetTechType))
+                KnownTechPatcher.RemovalTechs.Add(targetTechType);
+        }
+
+        #region StaticMethods
 
         /// <summary>
         /// Allows you to define which TechTypes are unlocked when a certain TechType is unlocked, i.e., "analysed".
@@ -77,6 +157,42 @@
             singleton.AddAnalysisTech(techTypeToBeAnalysed, techTypesToUnlock, UnlockMessage, UnlockSound, UnlockSprite);
         }
 
+
+        /// <summary>
+        /// Allows you to set up a custom Compound Unlock requiring multiple techtypes to be unlocked before 1 is.
+        /// ***Note: This will not remove any original unlock and if you need to do so you should use <see cref="RemoveAnalysisTechEntryFromSpecific"/> or <see cref="RemoveAllCurrentAnalysisTechEntry"/>
+        /// </summary>
+        /// <param name="techType"></param>
+        /// <param name="compoundTechsForUnlock"></param>
+        public static void SetCompoundUnlock(TechType techType, List<TechType> compoundTechsForUnlock)
+        {
+            singleton.AddCompoundUnlock(techType, compoundTechsForUnlock);
+        }
+
+
+        /// <summary>
+        /// Allows you to remove unlock entries for a <see cref="TechType"/> from specific entries.
+        /// </summary>
+        /// <param name="targetTechType">Target <see cref="TechType"/> to remove the unlocks for.</param>
+        /// <param name="techTypes">List of <see cref="TechType"/> to remove the targetTechType from.</param>
+        public static void RemoveAnalysisTechEntryFromSpecific(TechType targetTechType, List<TechType> techTypes)
+        {
+            singleton.RemoveAnalysisSpecific(targetTechType, techTypes);
+        }
+
+        /// <summary>
+        /// Allows you to remove all unlock entries from a <see cref="TechType"/> to be able to disable or change it to a new unlock.
+        /// ***Note: This is patch time specific so the LAST mod to call this on a techtype will be the only one to control what unlocks said type after its use.***
+        /// </summary>
+        /// <param name="targetTechType">Target <see cref="TechType"/> to remove the unlocks for.</param>
+        public static void RemoveAllCurrentAnalysisTechEntry(TechType targetTechType)
+        {
+            singleton.RemoveAnalysisTechEntry(targetTechType);
+        }
+
+        #endregion
+
+        #region InterfaceMethods
         /// <summary>
         /// Allows you to define which TechTypes are unlocked when a certain TechType is unlocked, i.e., "analysed".
         /// If there is already an exisitng AnalysisTech entry for a TechType, all the TechTypes in "techTypesToUnlock" will be
@@ -169,5 +285,38 @@
         {
             singleton.AddAnalysisTech(techTypeToBeAnalysed, techTypesToUnlock, "NotificationBlueprintUnlocked", UnlockSound, UnlockSprite);
         }
+
+        /// <summary>
+        /// Allows you to set up a custom Compound Unlock requiring multiple techtypes to be unlocked before 1 is.
+        /// ***Note: This will not remove any original unlock and if you need to do so you should use <see cref="IKnownTechHandler.RemoveAnalysisTechEntryFromSpecific"/> or <see cref="IKnownTechHandler.RemoveAllCurrentAnalysisTechEntry"/>
+        /// </summary>
+        /// <param name="techType"></param>
+        /// <param name="compoundTechsForUnlock"></param>
+        void IKnownTechHandler.SetCompoundUnlock(TechType techType, List<TechType> compoundTechsForUnlock)
+        {
+            singleton.AddCompoundUnlock(techType, compoundTechsForUnlock);
+        }
+
+
+        /// <summary>
+        /// Allows you to remove unlock entries for a <see cref="TechType"/> from specific entries.
+        /// </summary>
+        /// <param name="targetTechType">Target <see cref="TechType"/> to remove the unlocks for.</param>
+        /// <param name="techTypes">List of <see cref="TechType"/> to remove the targetTechType from.</param>
+        void IKnownTechHandler.RemoveAnalysisTechEntryFromSpecific(TechType targetTechType, List<TechType> techTypes)
+        {
+            singleton.RemoveAnalysisSpecific(targetTechType, techTypes);
+        }
+
+        /// <summary>
+        /// Allows you to remove all unlock entries from a <see cref="TechType"/> to be able to disable or change it to a new unlock.
+        /// ***Note: This is patch time specific so the LAST mod to call this on a techtype will be the only one to control what unlocks said type after its use.***
+        /// </summary>
+        /// <param name="targetTechType">Target <see cref="TechType"/> to remove the unlocks for.</param>
+        void IKnownTechHandler.RemoveAllCurrentAnalysisTechEntry(TechType targetTechType)
+        {
+            singleton.RemoveAnalysisTechEntry(targetTechType);
+        }
+        #endregion
     }
 }
--- SMLHelper/Patchers/KnownTechPatcher.cs
+++ SMLHelper/Patchers/KnownTechPatcher.cs
@@ -11,8 +11,9 @@
 
         internal static List<TechType> UnlockedAtStart = new List<TechType>();
         internal static IDictionary<TechType, KnownTech.AnalysisTech> AnalysisTech = new SelfCheckingDictionary<TechType, KnownTech.AnalysisTech>("AnalysisTech", AsStringFunction);
-
-        private static bool initialized = false;
+        internal static IDictionary<TechType, KnownTech.CompoundTech> CompoundTech = new SelfCheckingDictionary<TechType, KnownTech.CompoundTech>("CompoundTech", AsStringFunction);
+        internal static IDictionary<TechType, List<TechType>> RemoveFromSpecificTechs = new SelfCheckingDictionary<TechType, List<TechType>>("RemoveFromSpecificTechs", AsStringFunction);
+        internal static List<TechType> RemovalTechs = new List<TechType>();
 
         private static FMODAsset UnlockSound;
 
@@ -20,32 +21,38 @@
         {
             harmony.Patch(AccessTools.Method(typeof(KnownTech), nameof(KnownTech.Initialize)),
                 postfix: new HarmonyMethod(AccessTools.Method(typeof(KnownTechPatcher), nameof(KnownTechPatcher.InitializePostfix))));
-
-            harmony.Patch(AccessTools.Method(typeof(KnownTech), nameof(KnownTech.Deinitialize)),
-                postfix: new HarmonyMethod(AccessTools.Method(typeof(KnownTechPatcher), nameof(KnownTechPatcher.DeinitializePostfix))));
-        }
-
-        internal static void DeinitializePostfix()
-        {
-            initialized = false;
         }
 
         internal static void InitializePostfix()
         {
-            if (initialized)
-                return;
-            initialized = true;
+            foreach(TechType techType in UnlockedAtStart)
+            {
+                if (!KnownTech.Contains(techType))
+                    KnownTech.Add(techType, false);
+            }
 
-            UnlockedAtStart.ForEach(x => KnownTech.Add(x, false));
-
-            // Direct access to private fields made possible by https://github.com/CabbageCrow/AssemblyPublicizer/
-            // See README.md for details.
             List<KnownTech.AnalysisTech> analysisTech = KnownTech.analysisTech;
             IEnumerable<KnownTech.AnalysisTech> techToAdd = AnalysisTech.Values.Where(a => !analysisTech.Any(a2 => a.techType == a2.techType));
 
             foreach (KnownTech.AnalysisTech tech in analysisTech)
+            { 
+                foreach(TechType techType in RemovalTechs)
+                {
+                    if(tech.unlockTechTypes.Contains(techType))
+                        tech.unlockTechTypes.Remove(techType);
+                }
+
+                if(RemoveFromSpecificTechs.TryGetValue(tech.techType, out var types))
+                {
+                    foreach(TechType type in types)
+                        if(tech.unlockTechTypes.Contains(type))
+                            tech.unlockTechTypes.Remove(type);
+                }
+            }
+            
+            foreach (KnownTech.AnalysisTech tech in analysisTech)
             {
-                if (tech.unlockSound != null && tech.techType == TechType.BloodOil)
+                if (UnlockSound == null && tech.unlockSound != null && tech.techType == TechType.Lead)
                     UnlockSound = tech.unlockSound;
 
                 foreach (KnownTech.AnalysisTech customTech in AnalysisTech.Values)
@@ -53,7 +60,7 @@
                     if (tech.techType == customTech.techType)
                     {
                         if (customTech.unlockTechTypes != null)
-                            tech.unlockTechTypes.AddRange(customTech.unlockTechTypes);
+                            tech.unlockTechTypes.AddRange(customTech.unlockTechTypes.Where((x)=> !tech.unlockTechTypes.Contains(x)));
 
                         if (customTech.unlockSound != null)
                             tech.unlockSound = customTech.unlockSound;
@@ -75,7 +82,29 @@
                 if (tech.unlockSound == null)
                     tech.unlockSound = UnlockSound;
 
-                analysisTech.Add(tech);
+                if (!KnownTech.Contains(tech.techType))
+                    analysisTech.Add(tech);
+            }
+
+
+            List<KnownTech.CompoundTech> compoundTech = KnownTech.compoundTech;
+            IEnumerable<KnownTech.CompoundTech> compoundTechToAdd = CompoundTech.Values.Where(a => !compoundTech.Any(a2 => a.techType == a2.techType));
+
+            foreach (KnownTech.CompoundTech tech in compoundTech)
+            {
+                foreach (KnownTech.CompoundTech customTech in CompoundTech.Values)
+                {
+                    tech.dependencies = customTech.dependencies;
+                }
+            }
+
+            foreach (KnownTech.CompoundTech tech in compoundTechToAdd)
+            {
+                if (tech == null)
+                    continue;
+
+                if (!KnownTech.Contains(tech.techType))
+                    compoundTech.Add(tech);
             }
         }
     }
