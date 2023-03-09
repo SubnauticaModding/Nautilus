e02417d5e81efe84f3f603c384249c5ec6f5e317
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-03-08 20:16:47 -0500

    Create ColorModifier.cs

---
+++ SMLHelper/Utility/MaterialModifiers/ColorModifier.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace SMLHelper.Utility.MaterialModifiers;
+
+/// <summary>
+/// Applies a color
+/// </summary>
+public class ColorModifier : MaterialModifier 
+{
+    private Color color;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="color"></param>
+    public ColorModifier(Color color)
+    {
+        this.color = color;
+    }
+
+    /// <summary>
+    /// Applies changes to the material.
+    /// </summary>
+    /// <param name="material"></param>
+    /// <param name="renderer"></param>
+    /// <param name="materialType"></param>
+    protected sealed override void ApplyChangesToMaterial(Material material, Renderer renderer, MaterialUtils.MaterialType materialType)
+    {
+        material.color = color;
+    }
+
+    /// <summary>
+    /// Does not block shader conversions by default.
+    /// </summary>
+    /// <param name="material"></param>
+    /// <param name="renderer"></param>
+    /// <param name="materialType"></param>
+    /// <returns></returns>
+    public sealed override bool BlockShaderConversion(Material material, Renderer renderer, MaterialUtils.MaterialType materialType)
+    {
+        return false;
+    }
+}
