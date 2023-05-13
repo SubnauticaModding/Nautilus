5feb8e15547844cfa0780b58426f758153167921
EldritchCarMaker <eldritchcarmaker@gmail.com>
2023-05-12 22:41:03 -0500

    fix: correct the tab node language patching

--- Nautilus/Crafting/TabNode.cs
+++ Nautilus/Crafting/TabNode.cs
@@ -15,14 +15,14 @@ internal class TabNode : Node
     internal string DisplayName { get; set; }
     internal string Name { get; set; }
 
-    internal TabNode(string[] path, CraftTree.Type scheme, Sprite sprite, string modName, string name, string displayName) : base(path, scheme)
+    internal TabNode(string[] path, CraftTree.Type scheme, Sprite sprite, string modName, string name, string displayName, string language = "English") : base(path, scheme)
     {
         Sprite = sprite;
         DisplayName = displayName;
         Name = name;
 
         ModSprite.Add(new ModSprite(SpriteManager.Group.Category, $"{Scheme.ToString()}_{Name}", Sprite));
-        LanguagePatcher.AddCustomLanguageLine(modName, $"{Scheme.ToString()}Menu_{Name}", DisplayName);
+        LanguagePatcher.AddCustomLanguageLine($"{Scheme.ToString()}Menu_{Name}", DisplayName, language);
     }
 
 }
