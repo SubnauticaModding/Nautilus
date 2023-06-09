12eac274eff1ac2a227e1ab201d4ea88f0636a3f
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2023-06-09 12:15:14 -0400

    chore: Clean up files and suppress warnings (#401)
    * Follow proper naming conventions
    
    * Fix odd spacing issue
    
    * Suppress naming warnings in config example mod
    
    * Fix unusual compile time conditions
    
    * Remove unused using statements
    
    * Reformat some odd indentations

--- Nautilus/Assets/Gadgets/FabricatorGadget.cs
+++ Nautilus/Assets/Gadgets/FabricatorGadget.cs
@@ -6,7 +6,7 @@ using Nautilus.Utility;
 #if SUBNAUTICA
 using Sprite = Atlas.Sprite;
 #elif BELOWZERO
-    using Sprite = UnityEngine.Sprite;
+using Sprite = UnityEngine.Sprite;
 #endif
 
 namespace Nautilus.Assets.Gadgets;
--- Nautilus/Handlers/Enums/Extensions/EnumExtensions_TechGroup.cs
+++ Nautilus/Handlers/Enums/Extensions/EnumExtensions_TechGroup.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using Nautilus.Handlers;
 using Nautilus.Utility;
 
 // ReSharper disable once CheckNamespace
