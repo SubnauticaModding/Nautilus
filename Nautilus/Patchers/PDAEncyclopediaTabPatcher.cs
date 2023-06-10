f0e6dcd351950a9b858e15c906a491a6d8abc32f
Hamlet <jonahdb2@protonmail.com>
2023-06-09 20:28:25 -0400

    compile error fixes

--- Nautilus/Patchers/PDAEncyclopediaTabPatcher.cs
+++ Nautilus/Patchers/PDAEncyclopediaTabPatcher.cs
@@ -14,6 +14,6 @@ internal class PDAEncyclopediaTabPatcher
     [HarmonyPostfix]
     internal static void EncyTabAwakePostfix(uGUI_EncyclopediaTab __instance)
     {
-      ModDatabankHandler.Initialize(__instance);
+      ModDatabankHandler.Initialize();
     }
 }
