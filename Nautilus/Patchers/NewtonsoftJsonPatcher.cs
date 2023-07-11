51e7daafff3ba0068d881da706264a1678fc2962
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-11 01:41:08 -0400

    Proper checking for custom enums

--- Nautilus/Patchers/NewtonsoftJsonPatcher.cs
+++ Nautilus/Patchers/NewtonsoftJsonPatcher.cs
@@ -5,7 +5,8 @@ using Nautilus.Utility;
 using System.Reflection.Emit;
 using Nautilus.Handlers;
 using System.Linq;
-using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
+using System;
+using System.Reflection;
 
 namespace Nautilus.Patchers;
 
@@ -27,6 +28,8 @@ internal static class NewtonsoftJsonPatcher
             {
                 // load the text variable to the eval stack
                 yield return new CodeInstruction(OpCodes.Ldloc_S, (byte)6);
+                // load the type local variable (type of enum) to the eval stack
+                yield return new CodeInstruction(OpCodes.Ldloc_0, (byte) 6);
                 // call the IsEnumValueModded method
                 var func = AccessTools.Method(typeof(NewtonsoftJsonPatcher), nameof(NewtonsoftJsonPatcher.IsEnumValueModded));
                 yield return Transpilers.EmitDelegate(IsEnumValueModded);
@@ -41,11 +44,22 @@ internal static class NewtonsoftJsonPatcher
         InternalLogger.Log("NewtonsoftJsonPatcher.InitializeValuesAndNamesTranspiler succeeded: " + found);
     }
 
-    private static bool IsEnumValueModded(string text)
+    private static bool IsEnumValueModded(string text, Type enumType)
     {
-        InternalLogger.Warn(text);
-        bool isCustom = EnumBuilder<TechType>.CacheManager.ContainsKey(text);
-        InternalLogger.Log("Custom: " + isCustom);
+        if (!_cachedCacheManagerContainsKeyMethods.TryGetValue(enumType, out var containsKeyMethod))
+        {
+            var enumBuilderType = typeof(EnumBuilder<>).MakeGenericType(enumType);
+            var cacheManager = enumBuilderType.GetProperty("CacheManager", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
+            containsKeyMethod = AccessTools.Method(cacheManager.GetType(), "ContainsKey", new Type[] { typeof(string) });
+            _cachedCacheManagers.Add(enumType, cacheManager);
+            _cachedCacheManagerContainsKeyMethods.Add(enumType, containsKeyMethod);
+        }
+        bool isCustom = (bool)containsKeyMethod.Invoke(_cachedCacheManagers[enumType], new object[] { text });
         return isCustom;
     }
+
+    // key: enum type, value: cache manager instances
+    private static Dictionary<Type, object> _cachedCacheManagers = new();
+    // key: enum type, value: methodinfo of the cache manager containskey method
+    private static Dictionary<Type, MethodInfo> _cachedCacheManagerContainsKeyMethods = new();
 }
