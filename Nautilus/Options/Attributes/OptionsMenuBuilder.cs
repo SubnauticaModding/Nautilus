266a03741d199bee4dd4e30db50dd0b32c267aec
MrPurple6411 <mrpurple6411@gmail.com>
2023-10-04 00:07:29 -0600

    Fix for ConfigFile based Mod Options no longer showing in the menu on first load or when using the MenuOpened flag.

--- Nautilus/Options/Attributes/OptionsMenuBuilder.cs
+++ Nautilus/Options/Attributes/OptionsMenuBuilder.cs
@@ -103,7 +103,7 @@ internal class OptionsMenuBuilder<T> : ModOptions where T : ConfigFile, new()
         if (ConfigFileMetadata.MenuAttribute.LoadOn.HasFlag(MenuAttribute.LoadEvents.MenuOpened))
         {
             ConfigFileMetadata.Config.Load();
-            foreach(var option in options)
+            foreach(var option in new List<OptionItem>(Options))
             {
                 RemoveItem(option.Id);
             }
