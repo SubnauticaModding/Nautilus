83b68744d4292be8b34206f8bdde315725960c2a
MrPurple6411 <mrpurple6411@gmail.com>
2023-12-17 15:35:43 -0700

    Survival Handler Fixes and Improvements. (#509)
    Fix: Setting negative oxygen did not work right.
    Fix: Setting Negative Health would never kill and could glitch the player if reaching 0.
    Fix: InventoryUsables was list instead of HashSet
    Improvement: Added RunActionOnConsume(TechType techType, Action customAction, bool isEdible) to allow mods to send their own custom action to be performed when item is consumed.

--- Nautilus/Patchers/SurvivalPatcher.cs
+++ Nautilus/Patchers/SurvivalPatcher.cs
@@ -10,7 +10,7 @@ namespace Nautilus.Patchers;
 internal class SurvivalPatcher
 {
     internal static IDictionary<TechType, List<Action>> CustomSurvivalInventoryAction = new SelfCheckingDictionary<TechType, List<Action>>("CustomSurvivalInventoryAction", TechTypeExtensions.sTechTypeComparer);
-    internal static List<TechType> InventoryUseables = new();
+    internal static HashSet<TechType> InventoryUseables = new();
 
     internal static void Patch(Harmony harmony)
     {
