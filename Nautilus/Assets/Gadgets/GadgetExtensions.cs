b3a8aac8b906642af9df0bfa24fb54e478982fcb
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-12-31 22:33:15 -0500

    Fix more compiler errors

--- Nautilus/Assets/Gadgets/GadgetExtensions.cs
+++ Nautilus/Assets/Gadgets/GadgetExtensions.cs
@@ -217,9 +217,9 @@ public static class GadgetExtensions
     {
         customPrefab.AddOnRegister(() =>
         {
-            foreach ((Vector3 position, Vector3 eulerAngles, Vector3 scale) in spawnLocations)
+            foreach (var spawnLocation in spawnLocations)
             {
-                CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(customPrefab.Info.ClassID, position, eulerAngles, scale));
+                CoordinatedSpawnsHandler.RegisterCoordinatedSpawn(new SpawnInfo(customPrefab.Info.ClassID, spawnLocation.Position, spawnLocation.EulerAngles, spawnLocation.Scale));
             }
         });
 
