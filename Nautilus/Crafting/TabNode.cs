62885900acaff1d34e6410de2a0f20acd5fa3ebf
Metious <71298690+Metious@users.noreply.github.com>
2024-03-05 12:38:52 +0330

    Fixed localization not affecting tab nodes

--- Nautilus/Crafting/TabNode.cs
+++ Nautilus/Crafting/TabNode.cs
@@ -1,3 +1,6 @@
+using Nautilus.Handlers;
+using Nautilus.Utility;
+
 namespace Nautilus.Crafting;
 
 using Nautilus.Assets;
@@ -15,15 +18,25 @@ internal class TabNode : Node
     internal Sprite Sprite { get; set; }
     internal string DisplayName { get; set; }
     internal string Name { get; set; }
+    internal string Id { get; }
 
-    internal TabNode(string[] path, CraftTree.Type scheme, Sprite sprite, string name, string displayName, string language = "English") : base(path, scheme)
+    internal TabNode(string[] path, CraftTree.Type scheme, Sprite sprite, string name, string displayName) : base(path, scheme)
     {
         Sprite = sprite;
         DisplayName = displayName;
         Name = name;
+        Id = $"{Scheme.ToString()}_{Name}";
 
-        ModSprite.Add(new ModSprite(SpriteManager.Group.Category, $"{Scheme.ToString()}_{Name}", Sprite));
-        LanguagePatcher.AddCustomLanguageLine($"{Scheme.ToString()}Menu_{Name}", DisplayName, language);
+        ModSprite.Add(new ModSprite(SpriteManager.Group.Category, Id, Sprite));
+
+        if (!string.IsNullOrEmpty(displayName))
+        {
+            LanguageHandler.SetLanguageLine(Id, displayName);
+        }
+        else if (string.IsNullOrEmpty(Language.main.Get(name)))
+        {
+            InternalLogger.Warn($"Display name was not specified and no existing language line has been found for Tab node '{name}'.");
+        }
     }
 
 }
