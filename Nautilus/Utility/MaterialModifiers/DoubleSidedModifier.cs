7ec1bef39aa8e78dd4576b7e4fd8b99f7b5eb360
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2024-12-26 19:03:05 -0500

    Add 5 new material modifiers

---
+++ Nautilus/Utility/MaterialModifiers/DoubleSidedModifier.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Nautilus.Utility.MaterialModifiers;
+
+/// <summary>
+/// A material modifier that changes materials of the specified type to be double-sided.
+/// </summary>
+public class DoubleSidedModifier : MaterialModifier
+{
+    private readonly MaterialUtils.MaterialType[] _typesToAffect;
+    
+    /// <summary>
+    /// Changes all materials of the types defined in <paramref name="typesToAffect"/> to be double-sided.
+    /// </summary>
+    /// <param name="typesToAffect">Only materials with these types will be affected.</param>
+    public DoubleSidedModifier(params MaterialUtils.MaterialType[] typesToAffect)
+    {
+        _typesToAffect = typesToAffect;
+    }
+    
+    /// <summary>
+    /// Applies changes to the specified material.
+    /// </summary>
+    public override void EditMaterial(Material material, Renderer renderer, int materialIndex, MaterialUtils.MaterialType materialType)
+    {
+        var affect = false;
+        
+        foreach (var type in _typesToAffect)
+        {
+            if (materialType == type)
+            {
+                affect = true;
+            }
+        }
+
+        if (affect)
+        {
+            material.SetFloat(ShaderPropertyID._MyCullVariable, 0);
+        }
+    }
+}
