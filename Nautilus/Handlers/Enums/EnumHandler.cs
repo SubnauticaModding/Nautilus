5ba2de3355b1fe65942b716f159815014a040339
Metious <71298690+Metious@users.noreply.github.com>
2025-10-13 04:08:07 +0330

    Get rid of redundant warning

--- Nautilus/Handlers/Enums/EnumHandler.cs
+++ Nautilus/Handlers/Enums/EnumHandler.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Reflection.Emit;
 using Nautilus.Utility;
 
-// Resharper
+// ReSharper disable once CheckNamespace
 namespace Nautilus.Handlers;
 
 /// <summary>
