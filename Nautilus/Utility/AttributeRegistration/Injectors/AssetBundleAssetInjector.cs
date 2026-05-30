21bb5f30d390327a670159d27c939ef97a7a3dc3
jbeast291 <75239008+jbeast291@users.noreply.github.com>
2026-05-30 02:49:02 -0400

    Asset bundle injector improvements

--- Nautilus/Utility/AttributeRegistration/Injectors/AssetBundleAssetInjector.cs
+++ Nautilus/Utility/AttributeRegistration/Injectors/AssetBundleAssetInjector.cs
@@ -1,11 +1,19 @@
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
 namespace Nautilus.Utility.AttributeRegistration.Injectors;
 
-internal sealed class AssetBundleAssetInjector(AssetBundle[] bundles) : IDependencyArgumentInjector
+internal sealed class AssetBundleAssetInjector : IDependencyArgumentInjector
 {
+    private readonly Dictionary<string, AssetBundle> _bundles = new();
+    
+    internal AssetBundleAssetInjector(AssetBundle[] bundles)
+    {
+        bundles.ForEach(bundle => _bundles.Add(bundle.name, bundle));
+    }
+
     public bool TryInjectToArgument(InjectionContext context, out object value)
     {
         AssetLoadAttribute assetAttribute = context.parameterInfo.GetCustomAttribute<AssetLoadAttribute>(true);
@@ -15,21 +23,49 @@ internal sealed class AssetBundleAssetInjector(AssetBundle[] bundles) : IDepende
             return false;
         }
         
-        if(bundles == null) throw new Exception($"Asked to load asset {context.parameterInfo.Name} without providing a bundle for {context.attribute.registryID} registry");
+        if(_bundles.Count == 0) throw new InjectorException(context, $"Asked to load asset {context.parameterInfo.Name} without providing a bundle for {context.attribute.registryID} registry");
         
         string assetName = context.parameterInfo.Name;
-        if (assetAttribute.assetNameToLoad != null) // Attribute asset name gets priority over the argument name (if defined)
+        if (assetAttribute.AssetToLoad != null) // Attribute asset name gets priority over the argument name (if defined)
         {
-            assetName = assetAttribute.assetNameToLoad;
+            assetName = assetAttribute.AssetToLoad;
         }
 
-        object asset = null;
-        foreach (AssetBundle bundle in bundles)
+        if (_bundles.Count == 1 || (_bundles.Count > 1 && assetAttribute.BundleName == null))
         {
-            asset = bundle.LoadAsset(assetName, context.parameterInfo.ParameterType);
+            value = LoadBruteForce(assetName, context.parameterInfo.ParameterType);
+        }
+        else
+        {
+            value = LoadFromBundle(assetAttribute.BundleName, assetName, context.parameterInfo.ParameterType, context);
+        }
+        
+        if (value == null)
+        {
+            throw new InjectorException(context, $"Asset {assetAttribute.AssetToLoad} could not be loaded! Returned null for the given bundle(s).");
+        }
+        
+        return true;
+    }
+
+    private object LoadFromBundle(string bundleName, string assetName, Type assetType, InjectionContext context)
+    {
+        if (_bundles.TryGetValue(bundleName, out AssetBundle bundle))
+        {
+            return bundle.LoadAsset(assetName, assetType);
+        }
+        throw new InjectorException(context, $"Unknown AssetBundle: {bundleName}! The bundle name should match the internal name set within UnityEditor.");
+    }
+
+    private object LoadBruteForce(string assetName, Type assetType)
+    {
+        object asset = null;
+        foreach (AssetBundle bundle in _bundles.Values)
+        {
+            InternalLogger.Log(bundle.name);
+            asset = bundle.LoadAsset(assetName, assetType);
             if (asset != null) break;
         }
-        value = asset;
-        return true;
+        return asset;
     }
 }
