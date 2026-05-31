6d9a4a1c362013c5da6c74ce258cb3e44f4d3690
jbeast291 <75239008+jbeast291@users.noreply.github.com>
2026-05-31 00:09:51 -0400

    Example mod updated

--- Example mod/RegisterServiceExamples.cs
+++ Example mod/RegisterServiceExamples.cs
@@ -4,29 +4,28 @@ using Nautilus.Assets;
 using Nautilus.Assets.Gadgets;
 using Nautilus.Crafting;
 using Nautilus.Utility.AttributeRegistration;
+using Nautilus.Utility.AttributeRegistration.RegistryRequirements;
 
 namespace Nautilus.Examples;
-[BepInPlugin("com.snmodding.nautilus.registerevent", "Nautilus RegisterEvent Example Mod", Nautilus.PluginInfo.PLUGIN_VERSION)]
+[BepInPlugin(ModGuid, "Nautilus RegisterEvent Example Mod", Nautilus.PluginInfo.PLUGIN_VERSION)]
 [BepInDependency("com.snmodding.nautilus")]
-public class RegisterEventExamples  : BaseUnityPlugin
+public class RegisterServiceExamples  : BaseUnityPlugin
 {
-    // Register Events allow you to define Prefabs with less code
+    private const string ModGuid = "com.snmodding.nautilus.registerevent";
+    
+    // Register attributes allow you to define Prefabs (and more) with less code.
+    // At their bare bones, they let you execute code blocks without the need to store a reference somewhere to execute it later
+    // For this tutorial Prefabs are used with this registration system, but more can be used.
     [Register("MyCoolItem")]
     private static void RegisterMyCoolItem(PrefabInfo info)
     {
         CustomPrefab myPrefab = new CustomPrefab(info);
         myPrefab.Register();
         /*
-         This is the bare minimum for a Custom Prefab
+         This is the minimum for a Custom Prefab
          Later when we have Nautilus search for and execute this method,
-         It will pass in a PrefabInfo already assigned a TechType of the iD of the register.
+         It will pass in a PrefabInfo already assigned a TechType of the registry ID.
          So for this example myPrefab will have the TechType 'MyCoolItem'
-         */
-        /*
-         IMPORTANT: if your Registry's iD is the exact same from another mod,
-         your registry may fail to execute. Ensure you give it a unique name that won't cause,
-         any conflicts with other mods using the system. In most cases, if your TechType is
-         unique, you won't have to worry about this.
          */
     }
     
@@ -51,7 +50,7 @@ public class RegisterEventExamples  : BaseUnityPlugin
             })
             .WithFabricatorType(CraftTree.Type.Fabricator)
             .WithStepsToFabricatorTab("Personal", "Equipment");
-        // Although this can be crafted you won't be able to pick it up due to missing components
+        // (Although this can be crafted you won't be able to pick it up due to missing components)
         myPrefab.Register();
         /*
          In this example 2 things are showcased, MyCoolItem will have its RegisterMyCoolItem()
@@ -61,23 +60,32 @@ public class RegisterEventExamples  : BaseUnityPlugin
          This method has A TechType myCoolItem in the method Header. The name of the TechType 
          parameter "myCoolItem" is used internally to create a modded enum value for you, so
          there is no need to use the EnumHandler or store static instances of your TechTypes.
-         NOTE: a *dependency* does not always have to a modded TechType and vise versa. The
-         dependency is usually the same name as the modded TechType but this is not required
-         */
-        /*
-         NOTE: You can also depend on registries from other mods to load first. Simply put
-         whatever their registry uses as an ID into your dependency list and Nautilus will
-         handle the rest. This is also why your IDs have to be unique, as conflicts with other
-         mods can occur.
+         NOTE: a *dependency* does not always have to a modded TechType and vice versa. The
+         dependency is usually the same name as the modded TechType, but this is not required
          */
     }
     
+    // For cross mod dependencies, the syntax "modGUID::RegistryID" is used. Nautilus will execute your
+    // registry once the other mod's registry is loaded, which could be during their load phase
+    [RequireGuid("com.theplaguespreads.theredplague")]
+    // require GUID is an example of conditional registrations. [RequireGuid] is a built-in implementor,
+    // only executing when all defined Mod Guid's are present within the BepInEx Chainloader.
+    // This functionality can be extended however by implementing IRegistryRequirement on an attribute.
+    [Register("MyModdeditem", "com.theplaguespreads.theredplague::SomePrefab")]
+    private static void RegisterModdedItem(PrefabInfo info, TechType someTRPPrefab)
+    {
+        // some code :3
+        // This will not ever execute though as "SomePrefab" is not a valid prefab within TRP
+    }
+
     // Now, to have Nautilus execute your registries, you must use the RegistryEventUtils
     private void Awake()
     {
-        // Here we ask Nautilus to search our assembly and execute our registries
-        
-        /*
+        RegisterAttributeService service = new RegisterAttributeService(ModGuid);
+        service.AddBasicInjectors();// This adds several injectors built into Nautilus. 
+        // Custom injectors can be defined here and added as well, but this is not needed for most use cases
+        service.ExecuteAssemblyRegisterAttributes(Assembly.GetExecutingAssembly());
+        /* 
          If you need more debug info, enable Nautilus's Debug toggle in settings.
          RegistryEventUtils will log every time a registry is executed.
         */
@@ -85,8 +93,8 @@ public class RegisterEventExamples  : BaseUnityPlugin
     
     // Bonus Info Below
 
-    /*Register Events are not limited to prefabs however
-     they can execute anything and are useful in managing load order of your mod*/
+    // Register Events are not limited to prefabs however
+    // they can execute anything and are useful in managing load order of your mod
     [Register("MyCoolSounds", "SomeRegistryIWantToLoadFirst")]
     private static void RegisterMySounds()
     {
