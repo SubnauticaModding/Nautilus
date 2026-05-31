b7e0bfe32ebb5cc297a2eb026bf97a688464aee8
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-05-31 00:28:23 -0400

    Minor documentation updates & cleanup

--- Nautilus/Utility/AttributeRegistration/Injectors/IDependencyArgumentInjector.cs
+++ Nautilus/Utility/AttributeRegistration/Injectors/IDependencyArgumentInjector.cs
@@ -1,7 +1,7 @@
 namespace Nautilus.Utility.AttributeRegistration.Injectors;
 
 /// <summary>
-/// Represents an injector that can inject a dependent parameter into a method for execution.
+/// Represents an injector that can inject a required parameter into a method for execution.
 /// </summary>
 public interface IDependencyArgumentInjector
 {
@@ -9,8 +9,8 @@ public interface IDependencyArgumentInjector
     /// Check whether the argument is valid for this injector. If so, the result of the conversion is outed and the method returns true.
     /// Otherwise, if this injector cannot inject for this argument, this method returns false.
     /// </summary>
-    /// <param name="context">The InjectionContext for the given argument.</param>
-    /// <param name="result">the result to inject based on the argument. Is null when this method is false</param>
+    /// <param name="context">The <see cref="InjectionContext"/> for the given argument.</param>
+    /// <param name="result">The result to inject based on the argument. Is null when this method is false.</param>
     /// <returns>True if there is a valid injection for the current argument, otherwise false.</returns>
-    public bool TryInjectToArgument(InjectionContext context, out object result);
+    bool TryInjectToArgument(InjectionContext context, out object result);
 }
